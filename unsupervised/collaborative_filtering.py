d0639774a877f02d606f8f115e56b8f7cf7ef6db
Sixuan <sixuan@giphy.com>
2023-07-20 09:21:02 -0400

    Create collaborative_filtering.py

---
+++ unsupervised/collaborative_filtering.py
@@ -0,0 +1,72 @@
+#  Useful Values
+num_movies, num_users = Y.shape
+num_features = 100
+
+### Step one: Initialize the parameters and select the Adam optimizer.
+
+# Set Initial Parameters (W, X), use tf.Variable to track these variables
+tf.random.set_seed(1234) # for consistent results
+W = tf.Variable(tf.random.normal((num_users,  num_features),dtype=tf.float64),  name='W')
+X = tf.Variable(tf.random.normal((num_movies, num_features),dtype=tf.float64),  name='X')
+b = tf.Variable(tf.random.normal((1,          num_users),   dtype=tf.float64),  name='b')
+
+# Instantiate an optimizer.
+optimizer = keras.optimizers.Adam(learning_rate=1e-1)
+
+
+### Step two: Learn W, X, b using Adam algo (adaptive learning rate converge faster compared to normal GD)
+
+iterations = 200
+lambda_ = 1
+for iter in range(iterations):
+    # Use TensorFlow’s GradientTape
+    # to record the operations used to compute the cost 
+    with tf.GradientTape() as tape:
+
+        # Compute the cost (forward pass included in cost)
+        cost_value = cofi_cost_func_v(X, W, b, Ynorm, R, lambda_)
+
+    # Use the gradient tape to automatically retrieve
+    # the gradients of the trainable variables with respect to the loss
+    grads = tape.gradient( cost_value, [X,W,b] )
+
+    # Run one step of gradient descent by updating
+    # the value of the variables to minimize the loss.
+    optimizer.apply_gradients( zip(grads, [X,W,b]) )
+
+    # Log periodically.
+    if iter % 20 == 0:
+        print(f"Training loss at iteration {iter}: {cost_value:0.1f}")
+
+
+### Step three: Prediect users' rating on unrated movies
+
+# Make a prediction using trained weights and biases
+p = np.matmul(X.numpy(), np.transpose(W.numpy())) + b.numpy()
+
+#restore the mean
+pm = p + Ymean
+
+my_predictions = pm[:,0]
+
+# sort predictions
+ix = tf.argsort(my_predictions, direction='DESCENDING')
+
+for i in range(17):
+    j = ix[i]
+    if j not in my_rated:
+        print(f'Predicting rating {my_predictions[j]:0.2f} for movie {movieList[j]}')
+
+print('\n\nOriginal vs Predicted ratings:\n')
+for i in range(len(my_ratings)):
+    if my_ratings[i] > 0:
+        print(f'Original {my_ratings[i]}, Predicted {my_predictions[i]:0.2f} for {movieList[i]}')
+
+
+### (optiona) Step four: Extract movies that has more ratings or movies has higher avg ratings.
+### In practice we could enhance our prediction by ultilizing more information. 
+
+filter=(movieList_df["number of ratings"] > 20)
+movieList_df["pred"] = my_predictions
+movieList_df = movieList_df.reindex(columns=["pred", "mean rating", "number of ratings", "title"])
+movieList_df.loc[ix[:300]].loc[filter].sort_values("mean rating", ascending=False)
